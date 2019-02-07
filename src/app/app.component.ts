0aee39a1775de176260463fa801d086f54fc771e
manish sharma <manish@bodacious-server.com>
2019-02-07 17:42:12 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare var googleyolo: any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'signInAndDriveUploadDemo';
+
+  // onSignIn(googleUser) {
+  //   var profile = googleUser.getBasicProfile();
+  //   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  //   console.log('Name: ' + profile.getName());
+  //   console.log('Image URL: ' + profile.getImageUrl());
+  //   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  // }
+
+  onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+
+  //   ngOnInit(){
+  //     const hintPromise = googleyolo.hint({
+  //       supportedAuthMethods: [
+  //          "https://accounts.google.com"
+  //       ],
+  //       supportedIdTokenProviders: [
+  //       {
+  //          uri: "https://accounts.google.com",
+  //          clientId: "YOUR_GOOGLE_CLIENT_ID"
+  //       }
+  //      ]
+  //    });
+  //  }
 }
